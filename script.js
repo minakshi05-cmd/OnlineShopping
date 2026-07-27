fae415dea0630d38789cc0c886da7402a755cb00
minakshi05-cmd <mdesiti@gmail.com>
2026-07-28 00:12:28 +0530

    Initial Commit

---
+++ script.js
@@ -0,0 +1,21 @@
+document.getElementById("loginForm").addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+
+    if(email === "admin@gmail.com" && password === "admin123"){
+
+        document.getElementById("message").innerHTML = "Login Successful!";
+        document.getElementById("message").style.color = "green";
+
+    }
+    else{
+
+        document.getElementById("message").innerHTML = "Invalid Email or Password!";
+        document.getElementById("message").style.color = "red";
+
+    }
+
+});
